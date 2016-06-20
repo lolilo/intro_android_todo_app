d6a42e715af4ee8966ac2339ac1353049cbbf8a5
lilo <lilo@squareup.com>
2016-06-19 20:20:58 -0700

    long click listener, delete item from list

--- app/src/main/java/com/example/lilo/todoapp/MainActivity.java
+++ app/src/main/java/com/example/lilo/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lilo.todoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lvItems;
     EditText etEditText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         lvItems = (ListView) findViewById(R.id.lvItems);
         lvItems.setAdapter(aToDoAdapter);
         etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                aToDoAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 
     public void populateArrayItems() {

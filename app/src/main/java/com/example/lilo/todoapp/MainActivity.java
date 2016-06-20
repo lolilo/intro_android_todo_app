dd75a6cb680e77935f4d52a27f4955952a09e051
lilo <lilo@squareup.com>
2016-06-19 20:12:41 -0700

    hard code, populate todo item

--- app/src/main/java/com/example/lilo/todoapp/MainActivity.java
+++ app/src/main/java/com/example/lilo/todoapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.lilo.todoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aToDoAdapter);
+    }
+
+    public void populateArrayItems() {
+        todoItems = new ArrayList<String>();
+        todoItems.add("Item 1");
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
     }
 }

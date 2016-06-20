771eb01e4de08c8cf5b46efb61118fc6dcea07b9
lilo <lilo@squareup.com>
2016-06-20 00:21:28 -0700

    SQLite database handler

---
+++ app/src/main/java/com/example/lilo/todoapp/database/TodoItemDatabase.java
@@ -0,0 +1,46 @@
+package com.example.lilo.todoapp.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TodoItemDatabase extends SQLiteOpenHelper {
+    // Database Info
+    private static final String DATABASE_NAME = "todoListDatabase";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table Names
+    private static final String TABLE_TODO_ITEMS = "todo_items";
+
+    // Todo Table Columns
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "title";
+
+    public TodoItemDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Called when the database is created for the FIRST time.
+    // If a database already exists on disk with the same DATABASE_NAME, this method will NOT be called.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TODO_TABLE = "CREATE TABLE" + TABLE_TODO_ITEMS +
+                "(" +
+                    KEY_ID + " INTEGER PRIMARY KEY," +
+                    KEY_TITLE + " TEXT" +
+                ")";
+        db.execSQL(CREATE_TODO_TABLE);
+    }
+
+    // Called when the database needs to be upgraded.
+    // This method will only be called if a database already exists on disk with the same DATABASE_NAME,
+    // but the DATABASE_VERSION is different than the version of the database that exists on disk.
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion) {
+            // Simplest implementation is to drop all old tables and recreate them
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO_ITEMS);
+            onCreate(db);
+        }
+    }
+}

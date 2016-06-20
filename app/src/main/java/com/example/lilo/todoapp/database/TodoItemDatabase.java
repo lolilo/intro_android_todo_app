1b084b0f29529dd5632ca1fe67a2daa68141edea
lilo <lilo@squareup.com>
2016-06-20 01:16:42 -0700

    CRUD operations

--- app/src/main/java/com/example/lilo/todoapp/database/TodoItemDatabase.java
+++ app/src/main/java/com/example/lilo/todoapp/database/TodoItemDatabase.java
@@ -1,11 +1,21 @@
 package com.example.lilo.todoapp.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.lilo.todoapp.models.TodoItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoItemDatabase extends SQLiteOpenHelper {
     private static TodoItemDatabase sInstance;
+
+    private static final String TAG = TodoItemDatabase.class.getName();
 
     // Database Info
     private static final String DATABASE_NAME = "todoListDatabase";
@@ -35,8 +45,8 @@ public class TodoItemDatabase extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String CREATE_TODO_TABLE = "CREATE TABLE" + TABLE_TODO_ITEMS +
                 "(" +
-                    KEY_ID + " INTEGER PRIMARY KEY," +
-                    KEY_TITLE + " TEXT" +
+                KEY_ID + " INTEGER PRIMARY KEY," +
+                KEY_TITLE + " TEXT" +
                 ")";
         db.execSQL(CREATE_TODO_TABLE);
     }
@@ -50,6 +60,102 @@ public class TodoItemDatabase extends SQLiteOpenHelper {
             // Simplest implementation is to drop all old tables and recreate them
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO_ITEMS);
             onCreate(db);
+        }
+    }
+
+    public void addTodoItem(TodoItem todoItem) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_TITLE, todoItem.title);
+
+            db.insertOrThrow(TABLE_TODO_ITEMS, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Log.d(TAG, "Error while adding todo item to database");
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    public long updateTodoItem(TodoItem todoItem) {
+        SQLiteDatabase db = getWritableDatabase();
+        long todoItemId = -1;
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(KEY_TITLE, todoItem.title);
+
+            int rows = db.update(
+                    TABLE_TODO_ITEMS, values, KEY_ID + " = ?",
+                    new String[]{String.valueOf(todoItem.getId())}
+            );
+
+            if (rows == 1) {
+                String todoItemSelectQuery = String.format("SELECT %s FROM %s WHERE %s = ?",
+                        KEY_ID, TABLE_TODO_ITEMS, KEY_TITLE);
+                Cursor cursor = db.rawQuery(todoItemSelectQuery,
+                        new String[]{String.valueOf(todoItem.getId())});
+                try {
+                    if (cursor.moveToFirst()) {
+                        todoItemId = cursor.getInt(0);
+                        db.setTransactionSuccessful();
+                    }
+                } finally {
+                    if (cursor != null && cursor.isClosed()) {
+                        cursor.close();
+                    }
+                }
+            } else {
+                // todoItem with this title did not already exist, so insert new todoItem
+                todoItemId = db.insertOrThrow(TABLE_TODO_ITEMS, null, values);
+                db.setTransactionSuccessful();
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to add or update todo item");
+        } finally {
+            db.endTransaction();
+        }
+        return todoItemId;
+    }
+
+    public List<TodoItem> getAllTodoItems() {
+        List<TodoItem> todoItems = new ArrayList<>();
+
+        String TODO_ITEMS_SELECT_QUERY = String.format("SELECT  * FROM " + TABLE_TODO_ITEMS);
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(TODO_ITEMS_SELECT_QUERY, null);
+        try {
+            if (cursor.moveToFirst()) {
+                TodoItem newTodoItem = new TodoItem();
+                newTodoItem.title = cursor.getString(cursor.getColumnIndex(KEY_TITLE));
+                todoItems.add(newTodoItem);
+            }
+            while (cursor.moveToNext()) ;
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to get todo items from database");
+        } finally {
+            if (cursor != null && cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+        return todoItems;
+    }
+
+    public void deleteTodoItem(TodoItem todoItem) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            db.delete(TABLE_TODO_ITEMS, KEY_ID + " =?", new String[] {String.valueOf(todoItem.getId())});
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to delete todo item from database");
+        } finally {
+            db.endTransaction();
         }
     }
 }
---
+++ app/src/main/java/com/example/lilo/todoapp/models/TodoItem.java
@@ -0,0 +1,10 @@
+package com.example.lilo.todoapp.models;
+
+public class TodoItem {
+    private int id;
+    public String title;
+
+    public int getId() {
+        return id;
+    }
+}

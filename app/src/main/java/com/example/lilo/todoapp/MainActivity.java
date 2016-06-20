47abe3ef5995646781d1d4b6dceae562a8230a1d
lilo <lilo@squareup.com>
2016-06-19 20:16:27 -0700

    onClick event handler, add item to list

--- app/src/main/java/com/example/lilo/todoapp/MainActivity.java
+++ app/src/main/java/com/example/lilo/todoapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lilo.todoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> todoItems;
     ArrayAdapter<String> aToDoAdapter;
     ListView lvItems;
+    EditText etEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,17 @@ public class MainActivity extends AppCompatActivity {
         populateArrayItems();
         lvItems = (ListView) findViewById(R.id.lvItems);
         lvItems.setAdapter(aToDoAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
     }
 
     public void populateArrayItems() {
         todoItems = new ArrayList<String>();
         todoItems.add("Item 1");
         aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
     }
 }

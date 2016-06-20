955c210780f2140459428b945e5def28c7579ca7
lilo <lilo@squareup.com>
2016-06-19 20:58:14 -0700

    return to previous screen onSaveItem

--- app/src/main/java/com/example/lilo/todoapp/EditItemActivity.java
+++ app/src/main/java/com/example/lilo/todoapp/EditItemActivity.java
@@ -2,6 +2,7 @@ package com.example.lilo.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditItemActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class EditItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_item);
+    }
+
+    public void onSaveItem(View view) {
+        this.finish();
     }
 }
